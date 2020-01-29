8635698c785b1a832d0ba54634bb4352d6037d75
Ryan Haig <haigryan@gmail.com>
2020-01-29 00:17:43 -0700

    www-localhost now reads from graphql-localhost

--- src/graphql/envVars.js
+++ src/graphql/envVars.js
@@ -1,3 +1,3 @@
 module.exports = {
-    graphqlEndpoint: process.env.GRAPHQL_ENDPOINT || 'https://goals-graphql.herokuapp.com/'
+    graphqlEndpoint: process.env.GRAPHQL_ENDPOINT || 'http://localhost:4000' || 'https://goals-graphql.herokuapp.com/'
 }
