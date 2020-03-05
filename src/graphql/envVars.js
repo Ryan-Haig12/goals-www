af9e843237f6c0995052e5fd567f7c9eac508019
Ryan Haig <haigryan@gmail.com>
2020-03-04 22:54:43 -0700

    trying to get goals-www-dev to hit goals-graphql, not localhost

--- src/graphql/envVars.js
+++ src/graphql/envVars.js
@@ -1,7 +1,5 @@
-console.log('here!', process.env.DEV_ENV)
-
 module.exports = {
     //graphqlEndpoint: process.env.GRAPHQL_ENDPOINT || 'http://localhost:4000' || 'https://goals-graphql.herokuapp.com/'
-    graphqlEndpoint: process.env.DEV_ENV ? process.env.GRAPHQL_ENDPOINT : 'http://localhost:4000',
+    graphqlEndpoint: process.env.DEV_ENV === 'true' ? process.env.GRAPHQL_ENDPOINT : 'http://localhost:4000',
     graphqlWebservice: process.env.GRAPQL_WS || 'ws://localhost:4000/',
 }
