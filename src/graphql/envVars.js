5e9a5c7a24baa801750cb5aa62434d8108a11ff6
Ryan Haig <haigryan@gmail.com>
2020-03-05 21:10:41 -0700

    changing env vars up

--- src/graphql/envVars.js
+++ src/graphql/envVars.js
@@ -1,6 +1,11 @@
-if(process.env.NODE_ENV === 'production') {
-  module.exports = require('./vars/prod');
-} else {
-  console.log(process.env.NODE_ENV)
-  module.exports = require('./vars/dev');
+// if(process.env.NODE_ENV === 'production') {
+//   module.exports = require('./vars/prod');
+// } else {
+//   console.log(process.env.NODE_ENV)
+//   module.exports = require('./vars/dev');
+// }
+
+module.exports = {
+  graphqlWebservice: process.env.GRAPQL_WS || 'ws://localhost:4000/',
+  graphqlEndpoint: process.env.GRAPHQL_ENDPOINT || 'http://localhost:4000/'
 }
