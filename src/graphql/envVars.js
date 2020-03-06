a3a6279381055a7b3880780fb45556c20ad7273f
Ryan Haig <haigryan@gmail.com>
2020-03-05 21:20:03 -0700

    Purge cache

--- src/graphql/envVars.js
+++ src/graphql/envVars.js
@@ -1,11 +1,4 @@
-// if(process.env.NODE_ENV === 'production') {
-//   module.exports = require('./vars/prod');
-// } else {
-//   console.log(process.env.NODE_ENV)
-//   module.exports = require('./vars/dev');
-// }
-
 module.exports = {
-  graphqlWebservice: process.env.GRAPQL_WS || 'ws://localhost:4000/',
-  graphqlEndpoint: process.env.GRAPHQL_ENDPOINT || 'http://localhost:4000/'
+  graphqlWebservice: process.env.GRAPQL_WS,
+  graphqlEndpoint: process.env.GRAPHQL_ENDPOINT
 }
