bbc9adfaf0554ded35a8d79a87f124b5e107da6c
Ryan Haig <haigryan@gmail.com>
2020-05-06 20:35:04 -0600

    removed the unnecessary introduction message to the app

--- src/components/auth/UnAuthenticatedPage.js
+++ src/components/auth/UnAuthenticatedPage.js
@@ -33,23 +33,8 @@ const UnAuthenticatedPage = ({ loginUserAction, logoutUserAction }) => {
             </StyledAuthDiv>
             <br />
             <StyledTextDiv>
-                <p>Damn, so you're struggling a bit huh?</p>
-                <p>Not really eating as healthy as you'd like?</p>
-                <p>Too much TV and not enough exercise?</p>
-                <p>Not really going to church every Sunday?</p>
-                <p>Feel like you're drinking too much?</p>
-                <p>Well guess what mother fucker</p>
-                <p>That shits on you. You gatta make a path for yourself</p>
-                <p>Start running again</p>
-                <p>Stop eating so many goddamn pizza rolls</p>
-                <p>Go for a hike with your wife for fucks sake</p>
-                <p>Go shoot some hoops with your pals instead of going to the bar</p>
-                <p>Spend some time with your kid, jesus do I really have to say that?</p>
-                <p>That's all on you</p>
-                <p>However, humans are lazy, stupid, and sometimes need a push</p>
-                <p>This app is your push</p>
-                <p>Join with your buddies or compete against yourself</p>
-                <p>Do what you gatta do to be the best you you can be</p>
+                <h1>Welcome to Goals!</h1>
+                <p>In this game, you can team up and face off against your friends to use competition as motivation to improve your life one goal at a time</p>
             </StyledTextDiv>
         </StyledUnauthedPage>
     )
