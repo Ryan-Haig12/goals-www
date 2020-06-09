42deb1ef13e125c32131d731d19c6017c0002fe2
Ryan Haig <haigryan@gmail.com>
2020-06-08 20:43:21 -0600

    fix empty group modal button

--- src/components/groups/EmptyGroupStatePage.js
+++ src/components/groups/EmptyGroupStatePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 
 import * as Theme from '../syledComponents/Theme'
 
 const EmptyGroupStatePage = () => {
+    const history = useHistory()
     return (
         <Jumbotron style={{ top: '100px', position: 'relative', width: '80%', margin: 'auto', color: Theme.yellow, background: Theme.darkBlue }}>
             <h1 style={{ width: '90%', margin: 'auto' }} >You are not a member of any groups yet!</h1>
@@ -12,7 +14,7 @@ const EmptyGroupStatePage = () => {
             <Button 
                 variant="warning"
                 size="lg"
-                onClick={ () => console.log('ddd') }
+                onClick={ () => history.push('/createGroup') }
             >Create new Group!</Button>
         </Jumbotron>
     )
